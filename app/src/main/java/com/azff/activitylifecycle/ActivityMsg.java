baccca1b74377200a3b52774437f7e021d912899
zff <zffffz@163.com>
2016-07-23 10:33:33 +0800

    页面1增加文本框，通过按钮获取文本在第二页面中显示提示，页面2增加textView，提取textEdit中的文本并显示

--- app/src/main/java/com/azff/activitylifecycle/ActivityMsg.java
+++ app/src/main/java/com/azff/activitylifecycle/ActivityMsg.java
@@ -2,12 +2,30 @@ package com.azff.activitylifecycle;
 
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by lsx on 2016/7/22.
  */
 public class ActivityMsg extends AppCompatActivity {
+    private TextView textView;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_msg);
+        //获取第一个活动页面中的
+        String message = getIntent().getStringExtra(MainActivity.BOOKNAME_OF_CATEGORY_PHYSICAL);
+        //通过单击按钮，在第二个活动页面中显示提示信息
+        // Toast.makeText(ActivityMsg.this,message,Toast.LENGTH_LONG).show();
 
+        //查找textView
+        textView = (TextView)findViewById(R.id.activity_msg_edit_view);
+        //文本框中输入文本，通过按钮将该文本显示在第二个活动页面中的textView中
+        textView.setText(message);
+    }
 }
--- app/src/main/java/com/azff/activitylifecycle/MainActivity.java
+++ app/src/main/java/com/azff/activitylifecycle/MainActivity.java
@@ -6,22 +6,38 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private final String TAG = "MainActivity";
+    public static final String BOOKNAME_OF_CATEGORY_PHYSICAL = "bookName_of_category_physical";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d(TAG,"onCreate方法被执行了。");
+        //查找button
         Button button = (Button) findViewById(R.id.avtivity_main_button);
-
+        //链接文本框
+        final EditText editText = (EditText) findViewById(R.id.activity_main_edit_text);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+           /* public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this,ActivityMsg.class);
                 startActivity(intent);
+            }*/
+            public void onClick(View v) {
+//                Toast.makeText(MainActivity.this,"Button clicked",Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(MainActivity.this,ActivityMsg.class);
+                //将文本通过单击按钮传递到第二个活动页面，并显示提示信息
+                //intent.putExtra("name","追加第二个活动页面的文字信息");
+                //将文本框中的内容通过单击按钮获取并在第二个活动页面的显示提示
+                intent.putExtra(BOOKNAME_OF_CATEGORY_PHYSICAL, editText.getText().toString());
+                startActivity(intent);
+
+
             }
         });
 
